e32924762495cd20af411c70573e3785d42c94ff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-02 16:12:47 +0300

    Added the db url

--- farm_login_backend/migrations/env.py
+++ farm_login_backend/migrations/env.py
@@ -1,13 +1,14 @@
 import os
-
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
-
+from dotenv import find_dotenv, load_dotenv
 from models.models import Base
+from sqlalchemy import engine_from_config, pool
+
+load_dotenv(find_dotenv())
+
+MYSQL_URL = os.environ.get("MYSQL_URL")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -23,6 +24,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", )
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
